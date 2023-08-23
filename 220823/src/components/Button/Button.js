ebfab33c27558b564f9d1f3cec9831c280ac00d3
JOVITA_PAULIUTE <JPAULIUTE@GMAIL.com>
2023-08-23 11:15:06 +0300

    calculator

---
+++ 220823/src/components/Button/Button.js
@@ -0,0 +1,9 @@
+const Button = ({ click, content, style }) => {
+  return (
+    <button className={"btn btn-" + (style ? style : "light")} onClick={click}>
+      {content}
+    </button>
+  );
+};
+
+export default Button;
