07b7c9c26bbbf03c5c74575a9592608bcb363124
JOVITA_PAULIUTE <JPAULIUTE@GMAIL.com>
2023-10-03 14:17:01 +0300

    namespace

--- 2023-10-03/index.php
+++ 2023-10-03/index.php
@@ -7,3 +7,5 @@ $climate = new \League\CLImate\CLImate;
 $climate->red('Whoa now this text is red.');
 $climate->blue('Blue? Wow!');
 
+\Jovitos\Pc20231003\A::index();
+
---
+++ 2023-10-03/src/A.php
@@ -0,0 +1,8 @@
+<?php
+namespace Jovitos\Pc20231003;
+
+class A {
+    public static function index() {
+        echo 'Testas';
+    }
+}
